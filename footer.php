372f07a795fb3a8d653c01eaf534789b20151942
Abhishek Gupta <56722724+abhishekgupta7858@users.noreply.github.com>
2020-03-03 20:16:47 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+		<footer>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-sm-6">
+						<p>&copy; 2016 AC Developers</p>
+					</div>
+					<div class="col-sm-6" id="des">
+						<p>Designed &amp; Developed by: <a href="https://www.facebook.com/adesh.chauhan.5243">Adesh Chauhan</a></p>
+					</div>
+				</div>
+			</div>	
+		</footer>
+	</body>
+</html>
